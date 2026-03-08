c72cc37bc428b4c5883c0cb68a0406167bd1f789
Gabriel Fonseca <gabriel-hunter@hotmail.com>
2026-03-07 23:23:24 -0300

    Add Topic form

---
+++ src/website/src/website/form_topic.py
@@ -0,0 +1,50 @@
+from django import forms
+
+from .topic import Topic
+
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model = Topic
+        fields = ["materia", "nome", "link", "data_estudo", "index"]
+        widgets = {
+            "materia": forms.Select(
+                attrs={
+                    "class": "form-input",
+                    "placeholder": "Selecione uma máteria",
+                    "autofocus": False,
+                }
+            ),
+            "nome": forms.TextInput(
+                attrs={
+                    "class": "form-input",
+                    "placeholder": "Ex: Regra de três, Funções...",
+                    "autofocus": True,
+                }
+            ),
+            "link": forms.URLInput(
+                attrs={
+                    "class": "form-input",
+                    "placeholder": "Link (opcional)",
+                }
+            ),
+            "data_estudo": forms.DateInput(
+                attrs={
+                    "class": "form-input",
+                    "type": "date",
+                }
+            ),
+            "index": forms.NumberInput(
+                attrs={
+                    "class": "form-input",
+                    "min": 0,
+                }
+            ),
+        }
+        labels = {
+            "materia": "Matéria",
+            "nome": "Nome do tópico",
+            "link": "Link (opcional)",
+            "data_estudo": "Data para estudar (opcional)",
+            "index": "Ordem (index)",
+        }
