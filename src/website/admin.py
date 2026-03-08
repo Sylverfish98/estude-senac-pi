9e7696359f46a6fa06d8bff5b157097a5f7fe380
Fernando Martins <81760656+FernandoM52@users.noreply.github.com>
2026-03-07 23:21:29 -0300

    Update admin.py

--- src/website/admin.py
+++ src/website/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .materia import Materia
+from .topic import Topic
+
+
+@admin.register(Materia)
+class MateriaAdmin(admin.ModelAdmin):
+    list_display = ("nome", "usuario", "cor")
+    list_filter = ("usuario",)
+    search_fields = ("nome",)
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("nome", "materia", "data_estudo", "index", "is_completed")
+    list_filter = ("is_completed", "data_estudo", "materia__usuario")
+    search_fields = ("nome", "materia__nome")
+    ordering = ("data_estudo", "materia", "index", "id")
